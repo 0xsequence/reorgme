c9dc88d96b47eb0ce5abecaf1d84417bc775b2cf
Agustin Aguilar <Agusxrun@gmail.com>
2021-08-04 11:05:11 +0000

    Fix export bin

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,4 @@
-import yargs from 'yargs/yargs'
-import { hideBin } from 'yargs/helpers'
-import { Reorgme, ReorgmeDefaults } from './reorgme'
-
-yargs(hideBin(process.argv))
-  .options({
-    id: { type: 'number', default: ReorgmeDefaults.id }
-  })
-  .command("start", "creates and starts a new testnet blockchain", () => {}, async (args) => {
-    await new Reorgme({ id: args.id }).start()
-  })
-  .command("stop", "stops and removes a testnet blockchain", () => {}, async (args) => {
-    await new Reorgme({ id: args.id }).stop()
-  })
-  .command("pause", "pauses a testnet blockchain", () => {}, async (args) => {
-    await new Reorgme({ id: args.id }).pause()
-  })
-  .command("resume", "resumes a testnet blockchain", () => {}, async (args) => {
-    await new Reorgme({ id: args.id }).resume()
-  })
-  .command("fork", "forks the node 0 of the chain", () => {}, async (args) => {
-    await new Reorgme({ id: args.id }).fork()
-  })
-  .command("join", "joins the node 0 after forking it", () => {}, async (args) => {
-    await new Reorgme({ id: args.id }).join()
-  })
-  .command("logs", "show logs for all the nodes", () => {}, async (args) => {
-    await new Reorgme({ id: args.id }).logs()
-  })
-  .parse()
-
 export * from './reorgme'
 export * from './utils'
 export * from './easylistr'
+export * from './cli'
